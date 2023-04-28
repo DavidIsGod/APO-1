e19b47e5932a19e650d088fd53bbf51d7d16ab6f
DavidIsGod <troyaalejandro4@gmail.com>
2023-04-27 19:10:42 -0500

    Inicializacion repo

---
+++ Integradora 3/src/ui/Executable.java
@@ -0,0 +1,66 @@
+package ui;
+
+public class Executable {
+    
+    private static Scanner reader;
+    private controller controller;
+
+}
+
+    public Executable() {
+
+        reader = new Scanner(System.in);
+        controller = new controller();
+
+    }
+
+    public static void main (String[] args) {
+
+        Executable exe = new Executable();
+        exe.menu();
+    }
+
+    public void menu() {
+
+        System.out.println(" -------------- WELCOME TO READX ---------------------");
+
+        boolean cond = false;
+
+        while (!cond){
+
+            System.out.println("Enter which function you want to use");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
